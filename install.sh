6f9035c6666eb1700162c16add07a5d3557694f4
Eduardo Moreno <hmitsurugi@gmail.com>
2020-09-26 18:43:58 +0200

    Fix section_groups files in SimpleMenu.

--- install.sh
+++ install.sh
@@ -19,6 +19,9 @@ echo "Installing..."
 mkdir -p /media/data/local/share/xmame/sm_bridge
 tar -xzf sm_bridge.tgz -C /media/data/local/share/xmame/sm_bridge
 chown -R root:root /media/data/local/share/xmame/sm_bridge
+sed -i 's/xmame52\/xmame.SDL.52/sm_bridge\/romset.52/g' /media/data/local/home/.simplemenu/section_groups/*.ini
+sed -i 's/xmame69\/xmame.SDL.69/sm_bridge\/romset.69/g' /media/data/local/home/.simplemenu/section_groups/*.ini
+sed -i 's/xmame84\/xmame.SDL.84/sm_bridge\/romset.84/g' /media/data/local/home/.simplemenu/section_groups/*.ini
 
 dialog --msgbox 'Installation completed.' 8 32
 
